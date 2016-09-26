6fcadcef45f4a112810e2a52d055dd89fffdf09c
abilash26 <rabilash26@gmail.com>
2016-09-26 21:48:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,103 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles  = {
+    'articleone': {
+      title: 'Article One | Abilash R',
+      heading: 'Artile One',
+      date: 'Sept 5, 2016',
+      content:  ` <p>
+           Hello there, this is a short article about me, i am Abilash from chennai
+           and i am a mechanical engineer and i have a deep interest in programming
+           computers, technology, astrophotography, and i love to learn programming
+           language like Javascript, python , PHP etc.
+         </p>`},
+    'articletwo': {
+        title: 'About Me | Abilash R',
+        heading: 'About Me',
+        date: 'Sept 5, 2016',
+        content:  ` <p>
+             random text about me here   random text about me here   random text about me here   random text about me here
+                random text about me here   random text about me here   random text about me here   random text about me here
+                   random text about me here   random text about me here   random text about me here   random text about me here
+           </p>
+           <p>
+                 random text about me here   random text about me here   random text about me here   random text about me here
+                    random text about me here   random text about me here   random text about me here   random text about me here
+                       random text about me here   random text about me here   random text about me here   random text about me here
+            </p>
+            <p>
+                  random text about me here   random text about me here   random text about me here   random text about me here
+                     random text about me here   random text about me here   random text about me here   random text about me here
+                        random text about me here   random text about me here   random text about me here   random text about me here
+             </p>`},
+    'articlethree': {
+      title: 'Work Experience| Abilash R',
+      heading: 'Work Experience',
+      date: 'Sept 5, 2016',
+      content:  ` <p>
+           Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me
+            Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me
+             Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me
+         </p>
+         <p>
+         Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me
+          Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me
+           Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me
+          </p>
+          <p>
+          Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me
+           Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me
+            Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me  Hire Me
+           </p>`}
+         };
+
+
+function createTemplate (data) {
+  var title = data.title;
+  var date = data.date;
+  var heading = data.heading;
+  var content = data.content;
+
+  var htmlTemplate = `
+  <html>
+  <head>
+      <title>
+      ${title}
+      </title>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+      <div class = "content">
+        <div>
+          <a href="/">Home</a>     <a href= "/workexp">Work Experience</a>    <a href= "/aboutme">About  </a>     <a href= "https://www.facebook.com/imab26">Find me on Facebook </a>
+        </div>
+        <hr/>
+        <h3>
+          ${heading}
+        </h3>
+        <div>
+          ${date}
+        </div>
+        <div>
+            ${content}
+           </div>
+         </div>
+         </body>
+
+  </html>
+
+  `;
+  return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/:articlename', function (req, res){
+  var articlename = req.params.articlename;
+  res.send(createTemplate(articles[articlename]));
 });
 
 app.get('/ui/style.css', function (req, res) {
