0a10ccf33abe2a4c9ba5e4918b72a29bf9515e35
abilash26 <rabilash26@gmail.com>
2016-10-01 23:46:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,42 +1,59 @@
+// Counter code
 var button = document.getElementById('counter');
 
-button.onclick = function() {
+button.onclick = function () {
+
+    // Create a request object
     var request = new XMLHttpRequest();
-    request.onreadystatechange = function (){
-        if (request.readyState === XMLHttpRequest.DONE){
-            if (request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // Take some action
+          if (request.status === 200) {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();          
+          }
+      }  
+      // Not done yet
     };
     
-    request.open('GET','http://abilash26.imad.hasura-app.io/counter',true);
+    // Make the request
+    request.open('GET', 'http://abilash26.imad.hasura-app.io/counter', true);
     request.send(null);
 };
 
-
+// Submit name
 var submit = document.getElementById('submit_btn');
-submit.onclick = function(){
+submit.onclick = function () {
+  
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // Take some action
+          if (request.status === 200) {
+              // Capture a list of names and render it as a list
+              var names = request.responseText;
+              names = JSON.parse(names);
+              var list = '';
+              for (var i=0; i< names.length; i++) {
+                  list += '<li>' + names[i] + '</li>';
+              }
+              var ul = document.getElementById('namelist');
+              ul.innerHTML = list;   
+          }
+      }  
+      // Not done yet
+    };
+    
+    // Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET', 'http://abilash26.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);  
 
-var request = new XMLHttpRequest();
-request.onreadystatechange = function (){
-    if (request.readyState === XMLHttpRequest.DONE){
-        if (request.status === 200){
-           var names = request.responseText;
-           names = JSON.parse(names);
-    var list = '';
-    for (var i=0; i < names.length; i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list; 
-        }
-    }
-};
-var nameInput = document.getElementById('Name');
-var name = nameInput.value;
-request.open('GET','http://abilash26.imad.hasura-app.io/submit-name?name='+ name,true);
-request.send(null);
 };
