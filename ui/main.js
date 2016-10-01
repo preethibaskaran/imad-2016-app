2f0a9f5fead01d28c2a766d64ad1be41789d737b
abilash26 <rabilash26@gmail.com>
2016-10-01 22:18:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
-console.log('Loaded!');
+// writing script for counter
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onclick = funtion (){
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
