b41a4537c57ca0f77c1ad7492a8b226778dd7f82
abilash26 <rabilash26@gmail.com>
2016-11-06 22:40:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ function createTemplate (data) {
 
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-image: url('https://www.dropbox.com/s/sf4decpm2k0mvha/post-bg.jpg?dl=0')">
+    <header class="intro-header" style="background-image: url('http://www.tuscanhotel.com/img/home_bas.jpg')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
