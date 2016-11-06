e0a07ce1fab00063c04d73e541d7ca5e1bb9d345
abilash26 <rabilash26@gmail.com>
2016-11-06 22:38:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ function createTemplate (data) {
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <div class="post-heading">
                         <h1>${heading}</h1>
-                        <h2 class="subheading">Problems look mighty small from 150 miles up</h2>
+                    
                         <span class="meta">Posted by <a href="#">Abilash</a> ${date.toDateString()}</span>
                     </div>
                 </div>
